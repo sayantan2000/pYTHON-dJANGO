f97fd69a03a22610ec79015cccf7a5b164abd2c4
sayantan sadhukhan <sayantansk13@gmail.com>
2021-08-25 16:47:08 +0530

    updates model config,added superuser,product table added

--- S_Kart/shop/models.py
+++ S_Kart/shop/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    product_id=models.AutoField
+    product_name=models.CharField(max_length=50)
+    product_desc=models.CharField(max_length=550)
+    product_date=models.DateField()
